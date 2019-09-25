453ed1861d7744ca0960143390c29a82143b89b3
alexsandroferrao <alexsandr.ferrao@avenuecode.com>
2019-09-25 18:45:39 -0300

    colocando a barra faltante no script do zalenium e ajustando as dependencias.

--- src/test/java/testcases/resumo/DadosBasicosAssociadoTest.java
+++ src/test/java/testcases/resumo/DadosBasicosAssociadoTest.java
@@ -3,10 +3,7 @@ package testcases.resumo;
 import base.BaseTest;
 import db.CamposBanco;
 import db.DadosBasicosDb;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import pageObjects.resumo.DadosBasicosAssociadoPage;
 
 import java.io.IOException;
@@ -32,7 +29,7 @@ public class DadosBasicosAssociadoTest extends BaseTest {
     String grupoEconomicoBanco;
     String cboBanco;
 
-    @BeforeTest
+    @Test
     public void deveriaPegarAlgoTela() {
         DadosBasicosAssociadoPage dadosBasicosPage = new DadosBasicosAssociadoPage(driver);
 
@@ -47,56 +44,56 @@ public class DadosBasicosAssociadoTest extends BaseTest {
 
     }
 
-    @Test
+    @AfterTest
     public void deveriaVerificarNomeCompletoAssociado() throws IOException, SQLException {
         DadosBasicosDb dadosBasicosDb = new DadosBasicosDb();
         nomeCompletoBanco = dadosBasicosDb.retornarDadosBasicosDb(CamposBanco.NOME_COMPLETO);
         assertEquals(nomeCompletoBanco, nomeCompletoTela);
     }
 
-    @Test
+    @AfterTest
     public void deveriaVerificarDataNascimento() throws IOException, SQLException {
         DadosBasicosDb dadosBasicosDb = new DadosBasicosDb();
         dataNascimentoBanco = dadosBasicosDb.retornarDadosBasicosDb(CamposBanco.DATA_NASCIMENTO);
         assertEquals(dataNascimentoBanco, dataNascimentoTela);
     }
 
-    @Test
+    @AfterTest
     public void deveriaVerificarEstadoCivil() throws IOException, SQLException {
         DadosBasicosDb dadosBasicosDb = new DadosBasicosDb();
         estadoCivilBanco = dadosBasicosDb.retornarDadosBasicosDb(CamposBanco.ESTADO_CIVIL);
         assertEquals(estadoCivilBanco, estadoCivilTela);
     }
 
-    @Test
+    @AfterTest
     public void deveriaVerificarCpf() throws IOException, SQLException {
         DadosBasicosDb dadosBasicosDb = new DadosBasicosDb();
         cpfBanco = dadosBasicosDb.retornarDadosBasicosDb(CamposBanco.CPF);
         assertEquals(cpfBanco, cpfTela);
     }
 
-    @Test
+    @AfterTest
     public void deveriaVerificarRg() throws IOException, SQLException {
         DadosBasicosDb dadosBasicosDb = new DadosBasicosDb();
         rgBanco = dadosBasicosDb.retornarDadosBasicosDb(CamposBanco.RG);
         assertEquals(rgBanco, rgTela);
     }
 
-    @Test
+    @AfterTest
     public void deveriaVerificarAssociadoDesde() throws IOException, SQLException {
         DadosBasicosDb dadosBasicosDb = new DadosBasicosDb();
         associadoDesdeBanco = dadosBasicosDb.retornarDadosBasicosDb(CamposBanco.ASSOCIADO_DESDE);
         assertEquals(associadoDesdeBanco, associadoDesdeTela);
     }
 
-    @Test
+    @AfterTest
     public void deveriaVerificarGrupoEconomico() throws IOException, SQLException {
         DadosBasicosDb dadosBasicosDb = new DadosBasicosDb();
         grupoEconomicoBanco = dadosBasicosDb.retornarDadosBasicosDb(CamposBanco.GRUPO_ECONOMICO);
         assertEquals(grupoEconomicoBanco, grupoEconomicoTela);
     }
 
-    @Test
+    @AfterTest
     public void deveriaVerificarCbo() throws IOException, SQLException {
         DadosBasicosDb dadosBasicosDb = new DadosBasicosDb();
         cboBanco = dadosBasicosDb.retornarDadosBasicosDb(CamposBanco.CBO);
