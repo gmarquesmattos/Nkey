c582ab1c01af75e637d2dde9fbcd8a1b3c90b7d4
guilherme_mattos <!@QWaszz1>
2019-10-21 10:03:58 -0300

    correção da classe acessar()

--- src/main/java/pageObjects/retaguarda/RetaguardaPage.java
+++ src/main/java/pageObjects/retaguarda/RetaguardaPage.java
@@ -7,28 +7,24 @@ import pageObjects.home.HomePage;
 
 
 public class RetaguardaPage extends BasePage {
+    private By tabelaCategoria = By.xpath("//td[@id='1_s_2_l_SR_Type_New_']");
+    private By seletorVisibilidade = By.name("s_vis_div");
+    private By opcaoTodosMeusAtendimentos = By.xpath("//div[@id='_swecontent']//option[2]");
 
 
-   public RetaguardaPage(WebDriver driver) {
+    public RetaguardaPage(WebDriver driver) {
         super(driver);
     }
-   private By tabelaCategoria = By.cssSelector("div.siebui-view:nth-child(2) div.siebui-applet.siebui-collapsible-applet.siebui-list.Selected.siebui-active.siebui-applet-active.siebui-hilight:nth-child(3) span.AppletStyleParent77 div.NotSelected div.AppletHIListBorder.siebui-applet-content.siebui-collapsible.siebui-collapsible-applet-content:nth-child(4) div.ui-jqgrid.ui-widget.ui-widget-content.ui-corner-all div.ui-jqgrid-view div.ui-jqgrid-bdiv table.ui-jqgrid-btable:nth-child(2) tbody:nth-child(1) tr.ui-widget-content.jqgrow.ui-row-ltr.ui-state-highlight:nth-child(2) td:nth-child(4) > a.drilldown");
-   private By seletorVisibilidade = By.name("s_vis_div");
-   private By opcaoTodosMeusAtendimentos = By.xpath("//div[@id='_swecontent']//option[2]");
 
     public RetaguardaPage acessarCadastroCanais() {
         HomePage homePO = new HomePage(driver);
         homePO.selecionarMenuHome();
         clicar(seletorVisibilidade);
         clicar(opcaoTodosMeusAtendimentos);
-
         return this;
     }
 
-
     public RetaguardaPage entrarRetarguarda() {
-
-
         clicar(tabelaCategoria);
         return this;
 
